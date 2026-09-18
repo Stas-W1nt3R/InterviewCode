35baedb183ae52b769aaac45602528566190998e
Stanislav Kondratiev <stanislaw.comza@gmail.com>
2026-09-18 16:20:07 +0300

    add models and register them in django-admin

--- InterviewCode/InterviewCode/settings.py
+++ InterviewCode/InterviewCode/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,17 +29,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n2)rdeh4jn+9*t7%n0k&jbx0e$9ckj8cgh*i1qim#08plx)xo5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
 INSTALLED_APPS = [
+    'mysite',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,8 +79,12 @@ WSGI_APPLICATION = 'InterviewCode.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
--- InterviewCode/mysite/models.py
+++ InterviewCode/mysite/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+import uuid
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=40)
+    slug_id = models.SlugField(unique=True, default=uuid.uuid4, editable=False)
+    users = models.ManyToManyField(User, related_name='rooms', through='RoomParticipant')
+
+
+    class Status(models.TextChoices):
+        WAITING = 'WAITING', 'Ожидание'
+        PROCESSING = 'PROCESSING', 'В процессе'
+        COMPLETED = 'COMPLETED', 'Завершено'
+        CANCELED = 'CANCELED', 'Отменено'
+
+    status = models.CharField(choices=Status.choices, max_length=20, default=Status.WAITING)
+    task = models.ForeignKey('Task', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}, {self.slug_id}, {self.status}"
+
+    class Meta:
+        db_table = 'rooms'
+
+
+class RoomParticipant(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='participants')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE,related_name='participants')
+
+    class Roles(models.TextChoices):
+        INTERVIEWER = 'INTERVIEWER', 'Интервьюер'
+        CANDIDATE = 'CANDIDATE', 'Кандидат'
+
+    role = models.CharField(choices=Roles.choices, max_length=20, default='candidate')
+
+    def __str__(self):
+        return f"{self.user.username},  {self.role}, {self.room.slug_id}"
+
+    class Meta:
+        unique_together = ('room', 'user')
+        db_table = 'participants'
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}, {self.description}"
+
+    class Meta:
+        db_table = 'tasks'
+
+
+class TestCase(models.Model):
+    enter_values = models.TextField()
+    exit_values = models.TextField()
+    expected_values = models.TextField()
+    task = models.ForeignKey(Task, related_name='test_cases',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.task}, {self.enter_values}, {self.expected_values}, {self.exit_values}"
+
+    class Meta:
+        db_table = 'test_cases'
+
+
+class Solution(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    code = models.TextField()
+
+    class Status(models.TextChoices):
+        SUCCESSFUL = "SUCCESSFUL", "Успешно"
+        UNSUCCESSFUL = "UNSUCCESSFUL", "Не успешно"
+
+    status = models.CharField(choices=Status.choices, max_length=20, default=Status.UNSUCCESSFUL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.room}, {self.task}, {self.code}, {self.status}"
+
+    class Meta:
+        db_table = 'solutions'
